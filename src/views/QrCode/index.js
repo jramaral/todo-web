434ce4f1012acc7c6352c337055a64688f321725
jose.amaral <j.robertoamaral@gmail.com>
2020-08-18 18:19:07 -0300

    funcionalidade para gerar o qrcode

--- src/views/QrCode/index.js
+++ src/views/QrCode/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Qr from "qrcode.react";
 
 import * as S from "./styles";
 import api from "../../services/api";
@@ -12,8 +13,15 @@ export default function QrCode() {
       <Header />
       <S.Content>
         <h1>CAPTURE O QRCODE PELO APP</h1>
-        <S.QrCodeArea></S.QrCodeArea>
         <p>suas atividade serão sincronizadas com a do celular</p>
+        <S.QrCodeArea>
+          <Qr value="getmacaddress" size={350} />
+        </S.QrCodeArea>
+        <S.ValidationCode>
+          <span>Digite a numerção que apareceu no seu dispositivo</span>
+          <input type="text" />
+          <button type="button">SICRONIZAR</button>
+        </S.ValidationCode>
       </S.Content>
       <Footer />
     </S.Container>
--- src/views/QrCode/styles.js
+++ src/views/QrCode/styles.js
@@ -22,7 +22,42 @@ export const Content = styled.div`
   }
 `;
 export const QrCodeArea = styled.div`
-  background: red;
   width: 50%;
-  height: 350px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const ValidationCode = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+
+  span {
+    padding: 10px;
+  }
+  input {
+    height: 30px;
+    font-size: 16px;
+    padding: 3px;
+    outline: none;
+    border: solid 1px #e5791b;
+    text-align: center;
+  }
+  button {
+    height: 35px;
+    margin-top: 10px;
+    background: #e5791b;
+    border: none;
+    cursor: pointer;
+    color: #fff;
+    font-weight: bold;
+    font-size: 17px;
+    border-radius: 10px;
+    outline: none;
+
+    &:hover {
+      background: #20295f;
+    }
+  }
 `;
